37fb7c417cf026345d7653fb0972560dc14e15a0
dannyradden <dradden@gmail.com>
2017-03-22 20:16:04 -0600

    Still woprking on erb

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -8,7 +8,7 @@ class Condition < ActiveRecord::Base
   validates :mean_humidity, presence: true
   validates :mean_visibility, presence: true
   validates :mean_wind_speed, presence: true
-  
+
     def self.create_condition(params)
       Condition.create(
         date: params[:condition][:date],
@@ -34,5 +34,42 @@ class Condition < ActiveRecord::Base
         precipitation: params[:condition][:precipitation]
       )
     end
+
+    def self.average_number_of_trips_for_condition(condition, low, high)
+      if Condition.condition_days(condition, low, high).count != 0
+        Condition.trips_for_each_day(condition, low, high).flatten.count / Condition.condition_days(condition, low, high).count
+      else
+        "No days applicable"
+      end
+    end
+
+      def self.trips_for_each_day(condition, low, high)
+        Condition.condition_days(condition, low, high).map(&:trips)
+      end
+
+      def self.condition_days(condition, low, high)
+        Condition.where(condition => low..high)
+      end
+
+    def self.most_trips_in_a_day_for_condition(condition, low, high)
+      if Condition.condition_days(condition, low, high).count != 0
+        Condition.trip_count_for_each_day(condition, low, high).max
+      else
+        "No days applicable"
+      end
+    end
+
+      def self.trip_count_for_each_day(condition, low, high)
+        Condition.condition_days(condition, low, high).map(&:trips.count)
+      end
+
+    def self.least_trips_in_a_day_for_condition(condition, low, high)
+      if Condition.condition_days(condition, low, high).count != 0
+        Condition.trip_count_for_each_day(condition, low, high).min
+      else
+        "No days applicable"
+      end
+    end
+
 
 end
